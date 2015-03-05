2e10cee0ccd07758b464ae045bb72c3a18047e79
Yoshimura Yuu <m@yyu.pw>
2015-03-05 13:40:11 +0900

    add gcd

--- BigInteger.hx
+++ BigInteger.hx
@@ -712,6 +712,34 @@ class BigInteger {
 		if(d.signum() < 0) return d.add(m); else return d;
 	}
 
+	// (public) gcd(this,a) (HAC 14.54)
+	public function gcd(a : BigInteger) {
+		var x = (this.s<0)?this.negate():this.clone();
+		var y = (a.s<0)?a.negate():a.clone();
+		if(x.compareTo(y) < 0) { var t = x; x = y; y = t; }
+		var i = x.getLowestSetBit(), g = y.getLowestSetBit();
+		if(g < 0) return x;
+		if(i < g) g = i;
+		if(g > 0) {
+			x.rShiftTo(g,x);
+			y.rShiftTo(g,y);
+		}
+		while(x.signum() > 0) {
+			if((i = x.getLowestSetBit()) > 0) x.rShiftTo(i,x);
+			if((i = y.getLowestSetBit()) > 0) y.rShiftTo(i,y);
+			if(x.compareTo(y) >= 0) {
+				x.subTo(y,x);
+				x.rShiftTo(1,x);
+			}
+			else {
+				y.subTo(x,y);
+				y.rShiftTo(1,y);
+			}
+		}
+		if(g > 0) y.lShiftTo(g,y);
+		return y;
+	}
+
 	public function new<T> (?a_ : Null<AbsNumber<T>>, ?b : Int = 10, ?c) {
 		var a : Null<Number<T>> = a_;
 		
